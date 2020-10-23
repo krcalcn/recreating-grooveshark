abdd3035671d03cd8466252f28104751778e9919
krcalcn <alicn.karaca@gmail.com>
2020-10-23 23:16:06 +0300

    Quasar framework initialize & Layout design

---
+++ frontend/src/layouts/MainLayout.vue
@@ -0,0 +1,267 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated class="bg-grad">
+      <q-toolbar>
+        <q-btn flat icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
+        <q-toolbar-title>
+           <q-btn
+            flat
+            class="text-h5 q-px-md"
+            label="Grooveshark"
+            icon="music_video"
+            to="/" no-caps />
+        </q-toolbar-title>
+
+        <q-toolbar-title align="right">
+        <!--  <q-btn
+          dense
+          label="User"
+          :icon="playerToggle ? 'expand_more' : 'expand_less'"
+          @click="playerToggle = !playerToggle"
+        />  -->
+        <q-btn text-color="white" label="Add Song" class="q-mr-md bg-content" />
+
+        <q-btn-dropdown auto-close flat label="User" class="bg-content">
+          <q-list padding style="width: 250px">
+            <q-item clickable>
+              <q-item-section avatar>
+                <q-avatar icon="account_circle" size="40px" font-size="40px" text-color="primary" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Profile</q-item-label>
+                <q-item-label caption>{{ new Date(0) }}</q-item-label>
+              </q-item-section>
+              <!-- <q-item-section side>
+                <q-icon name="info" color="amber" />
+              </q-item-section> -->
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      content-class="bg-dark text-white"
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view class="bg-content"/>
+    </q-page-container>
+
+    <q-footer
+      elevated
+      class="bg-grad-player text-white row">
+      <div class="col-12 row">
+        <q-toolbar-title class="row">
+
+          <div
+            class="col-3 player-clickable"
+            @click="playerToggle = !playerToggle">
+          </div>
+          <div class="col-6 row q-mt-sm q-my-md  play">
+            <div class="col-2 q-py-sm q-pl-md">
+              <q-btn
+                color="primary"
+                flat
+                round
+                icon="fast_rewind"
+                size="md"
+                @click="() => {
+                  wavesurfer.skipBackward(1);
+                  wavesurfer.play();
+                  isPlaying = true;
+                  }"
+              />
+              <q-btn
+                color="primary"
+                round
+                :icon="isPlaying ? 'pause' : 'play_arrow'"
+                size="md"
+                @click="() => {
+                  wavesurfer.playPause();
+                  isPlaying = !isPlaying;
+                  }"
+              />
+              <q-btn
+                color="primary"
+                flat
+                round
+                icon="fast_forward"
+                size="md"
+                @click="() => {
+                  wavesurfer.skipForward(1)
+                  wavesurfer.play();
+                  isPlaying = true;
+                  }"
+              />
+            </div>
+
+            <div class="col-10">
+              <div
+                id="waveform"
+                class="col-12 q-ml-md"
+                @click="isPlaying ? wavesurfer.play() : ''">
+                </div>
+            </div>
+          </div>
+          <q-space class="player-clickable" @click="playerToggle = !playerToggle"/>
+          <div
+            class="player-clickable"
+            @click="playerToggle = !playerToggle"></div>
+          <q-btn
+            :icon="playerToggle ? 'expand_more' : 'expand_less'"
+            @click="playerToggle = !playerToggle"
+            class="q-ma-md bg-grad-btn"
+          />
+        </q-toolbar-title>
+      </div>
+      <transition
+        appear
+        enter-active-class="animated slideInUp"
+        leave-active-class="animated slideOutDown">
+
+        <q-toolbar v-if="playerToggle" class="bg-grad queue">
+        </q-toolbar>
+
+      </transition>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue';
+import WaveSurfer from 'wavesurfer.js';
+import axios from 'axios';
+
+const linksData = [
+  {
+    title: 'My Musics',
+    icon: 'school',
+    link: 'https://quasar.dev',
+  },
+  {
+    title: 'My Favorites',
+    icon: 'code',
+    link: 'https://github.com/quasarframework',
+  },
+  {
+    title: 'Playlists',
+    icon: '',
+    link: 'https://github.com/quasarframework',
+  },
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+      playerToggle: true,
+      wavesurfer: null,
+      song: null,
+      isPlaying: false,
+    };
+  },
+  mounted() {
+    if (!this.wavesurfer) this.createWaveSurfer();
+    axios
+      .get('http://localhost:3000/users/track/5f8dfcfb30926bb64406a077', {
+        headers: {
+          'Access-Control-Allow-Origin': true,
+        },
+      })
+      .then((data) => {
+        this.song = data.config.url;
+        console.log(data);
+      });
+    window.addEventListener('keyup', (event) => {
+      if (event.keyCode == 32) {
+        this.wavesurfer.playPause();
+        this.isPlaying = !this.isPlaying;
+      }
+    });
+    window.addEventListener('keydown', (event) => {
+      if (event.keyCode == 17) {
+        console.log('ctrl');
+        window.addEventListener('keydown', (e) => {
+          if (e.keyCode == 39) {
+            this.wavesurfer.skipForward(1);
+          }
+          if (e.keyCode == 37) {
+            this.wavesurfer.skipBackward(1);
+          }
+        });
+      }
+    });
+  },
+  methods: {
+    createWaveSurfer() {
+      this.wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        barWidth: 1,
+        waveColor: 'white',
+        progressColor: '#F05020',
+        scrollParent: false,
+        barGap: 3,
+        barHeight: 1.5,
+        cursorColor: '#FFFFFF',
+        cursorWidth: 2.5,
+        height: 40,
+        hideScrollbar: true,
+        mediaControls: true,
+        responsive: true,
+      });
+      this.wavesurfer.load('http://localhost:3000/users/track/5f8dfcfb30926bb64406a077');
+      this.wavesurfer.on('ready', () => {
+
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+  .bg-grad-player {
+    background: linear-gradient(to bottom, #202020, #424242)
+  }
+  .bg-grad {
+    background: linear-gradient(to bottom, #202020, #323232)
+  }
+  .player-clickable {
+    cursor: pointer;
+  }
+  .bg-grad-btn {
+    background: linear-gradient(to bottom, #353535, #202020)
+  }
+  .bg-content {
+    background: linear-gradient(to bottom, #F2A62F ,#F57028)
+  }
+  .queue {
+    min-height: 150px;
+  }
+  #waveform {
+    cursor: pointer;
+    margin-top: 10px;
+  }
+  .play {
+    box-shadow: inset 0px 0px 20px #121212;
+  }
+</style>
